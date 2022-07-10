78e5c692e2c38d5cc204f4b87901da329df6446d
djmatt18000 <43975438+djmatt18000@users.noreply.github.com>
2022-07-10 03:53:08 -0500

    run matt ai

---
+++ RunMattAI.py
@@ -0,0 +1,14 @@
+from tkinter import Tk
+
+# my code
+window = Tk()
+
+window.title("MattAI V2.2.7.5")
+
+window.configure(width=650, height=450)
+
+window.configure(bg='lightgray')
+window.mainloop()
+# yeah my code is bad
+
+print("MattAi Working")
